213c2241c285e3f96092d4a7f6963149b124b00b
Nelson (Nelx) Secco Filho <nelsonseccofilho@gmail.com>
2018-12-09 10:45:00 -0200

    getData from newsApi

---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+import { NewsPage } from '../news/news.page';
+import { Tab2Page } from '../tab2/tab2.page';
+import { Tab3Page } from '../tab3/tab3.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirectTo: '/tabs/(news:news)',
+        pathMatch: 'full',
+      },
+      {
+        path: 'news',
+        outlet: 'news',
+        component: NewsPage
+      },
+      {
+        path: 'tab2',
+        outlet: 'tab2',
+        component: Tab2Page
+      },
+      {
+        path: 'tab3',
+        outlet: 'tab3',
+        component: Tab3Page
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/(news:news)',
+    pathMatch: 'full',
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
